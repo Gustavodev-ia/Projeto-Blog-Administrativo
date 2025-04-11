602f2cad68c268460a8dcf56f68952670e595229
Gustavo Rodrigues <gustavo.desenvolvedor88@gmail.com>
2025-04-11 07:55:56 -0300

    Configuracao inicial do backend

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+require("dotenv").config()
+
+app.use(express.json())
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Servidor rodando na porta ${process.env.PORT}`)
+})
